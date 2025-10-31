f902207a5eeb4f250440199147b2b4749254244c
Arjun Ghadage <157689200+ArjunG01@users.noreply.github.com>
2025-10-31 12:20:35 +0530

    Delete StudentsManagementSystem directory

--- StudentsManagementSystem/src/main/java/com/StudentsManagementSystem/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StudentsManagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.StudentsManagementSystem.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findByUsername(String username); // For login
-}
